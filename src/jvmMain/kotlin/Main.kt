ca5fd976d7caa109401527f60ce4db3b6dedc388
Kamil Kras <38427679+xVemu@users.noreply.github.com>
2025-09-10 19:22:17 +0200

    Initial commit

---
+++ src/jvmMain/kotlin/Main.kt
@@ -0,0 +1,5 @@
+fun main() {
+    print("test main")
+}
+
+fun add(a: Int, b: Int) = a + b
---
+++ src/nativeTest/kotlin/Main.kt
@@ -0,0 +1,10 @@
+import io.kotest.matchers.shouldBe
+import kotlin.test.Test
+
+class Main {
+    @Test
+    fun `test add`() {
+        val res = add(1, 2)
+        res shouldBe 3
+    }
+}
