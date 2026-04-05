13b166890cea103d8458708f5d76cbf289ec2922
Kamil Kras <38427679+xVemu@users.noreply.github.com>
2026-04-05 14:09:09 +0200

    Migrate to Amper

--- build.gradle.kts
+++
@@ -1,6 +0,0 @@
-plugins {
-    // this is necessary to avoid the plugins to be loaded multiple times
-    // in each subproject's classloader
-    alias(libs.plugins.kotlin.multiplatform) apply false
-    alias(libs.plugins.kotlin.jvm) apply false
-}
